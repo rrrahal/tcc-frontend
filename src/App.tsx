daae8c230079cd8e43c74eea9bdf9da2380b353b
Rafael Rahal <rrrahal@gmail.com>
2023-11-25 14:14:30 -0300

    add lollipop chart

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import { Header } from './components/ui/header'
 import { Description } from './components/ui/description'
 import { Map } from './components/map'
 import { data } from './data/map_data'
+import { words_data } from './data/words'
 import CountriesData from './data/countries.json'
+import { Lollipop } from './components/vis/lollipop'
 
 export enum SelectedView {
   VentureModel,
@@ -13,10 +15,18 @@ export enum SelectedView {
 function App() {
   const [selectedView, setSelectedView] = React.useState(SelectedView.VentureModel)
 
+  const viz = (
+    <div className='flex flex-col items-center justify-center w-full h-full'>
+      <Map width={700} height={400} data={data} countries={CountriesData} />
+      <Lollipop width={700} height={400} data={words_data} />
+    </div>
+  )
+
+
   return (
     <div className='app min-w-[100vw] min-h-[100vh]'>
       <Header setSelectedView={setSelectedView} />
-      {selectedView === SelectedView.VentureModel ? <Description /> : <Map width={700} height={400} data={data} countries={CountriesData} />}
+      {selectedView === SelectedView.VentureModel ? <Description /> : viz}
     </div>
   )
 }
